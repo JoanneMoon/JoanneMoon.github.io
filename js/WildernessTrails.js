4f877b6991d18b1906e926d8fa88a54cec3e1341
JoanneMoon <JoanneMoon@users.noreply.github.com>
2020-10-05 15:07:02 -0500

    Header footer references

--- js/WildernessTrails.js
+++ js/WildernessTrails.js
@@ -3,6 +3,6 @@
 
 // Use a common header and footer
 $(function () {
-    $("#header").load("header.html");
-    $("#footer").load("footer.html");
+    $("#header").load("/header.html");
+    $("#footer").load("/footer.html");
 });
